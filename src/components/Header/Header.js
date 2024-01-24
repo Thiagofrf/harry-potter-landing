949f8ba6e643ce09162fda56868338692cf29c44
Thiago Fontanella <thiago.fontanella@hotmail.com>
2024-01-23 23:14:16 -0300

    feature: finish header mockup (provisory colors and links); add sass

---
+++ src/components/Header/Header.js
@@ -0,0 +1,25 @@
+import './style.scss'
+import Logo from '../../assets/hogwarts-icon.png'
+
+const Header = () => {
+    return (
+        <header className="hp-header">
+            <div className='hp-header__logo'>
+                <img src={Logo} alt='Harry Potter - Símbolo da casa Grifinória'/>
+            </div>
+            <div className='hp-header__links'>
+                <a className='hp-header__links--item'>
+                    Sobre 
+                </a>
+                <a className='hp-header__links--item'>
+                    Casas
+                </a>
+                <a className='hp-header__links--item'>
+                    Personagens
+                </a>
+            </div>
+        </header>
+    )
+}
+
+export default Header
